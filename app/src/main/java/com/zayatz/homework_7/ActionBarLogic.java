b712807e75c66e6b1b888062c51f729326e9a57b
Andrew Zayatz <zayatzandrew@mail.ru>
2016-03-05 14:57:54 +0200

    ex1

---
+++ app/src/main/java/com/zayatz/homework_7/ActionBarLogic.java
@@ -0,0 +1,10 @@
+package com.zayatz.homework_7;
+
+/**
+ * Created by Zayatz on 05.03.2016.
+ */
+public abstract class ActionBarLogic {
+    public interface OnMenuVisibilityListener {
+        public void onMenuVisibilityChanged(boolean isVisible);
+    }
+}
--- app/src/main/java/com/zayatz/homework_7/MainActivity.java
+++ app/src/main/java/com/zayatz/homework_7/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zayatz.homework_7;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,25 +10,29 @@ import android.text.style.ImageSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActionBar.OnMenuVisibilityListener {
+
+    private TextView tvItemBar1, tvItemBar2, tvItemBar3, tvItemText1, tvItemText2, tvItemText3;
+    private Button btnItemMenu1, btnItemMenu2, btnItemMenu3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar bar = getActionBar();
+        bar.addOnMenuVisibilityListener(this);
+        findViews();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.ab_menu_am, menu);
-        MenuItem item1 = menu.findItem(R.id.ab_item1);
-        MenuItem item2 = menu.findItem(R.id.ab_item2);
-        MenuItem item3 = menu.findItem(R.id.ab_item3);
-
         Log.d("TAG", "onCreate");
-        //uncheckOther(item1, item2, item3);
-
         return true;
     }
 
@@ -53,10 +58,48 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
     private void sendStateByIntent (MenuItem item) {
         Intent intent = new Intent();
         intent.putExtra(Constant.EXTRA_ITEM_STATE, true);
         item.setIntent(intent);
+    }
+
+    private  void findViews() {
+        tvItemBar1 = (TextView) findViewById(R.id.itemBar1_AM);
+        tvItemBar2 = (TextView) findViewById(R.id.itemBar2_AM);
+        tvItemBar3 = (TextView) findViewById(R.id.itemBar3_AM);
+
+        tvItemText1 = (TextView) findViewById(R.id.itemText1_AM);
+        tvItemText2 = (TextView) findViewById(R.id.itemText2_AM);
+        tvItemText3 = (TextView) findViewById(R.id.itemText3_AM);
+
+        btnItemMenu1 = (Button) findViewById(R.id.itemButton1_AM);
+        btnItemMenu2 = (Button) findViewById(R.id.itemButton2_AM);
+        btnItemMenu3 = (Button) findViewById(R.id.itemButton3_AM);
+    }
+
+    private void itemSetEnabled (int item, boolean enable) {
+        switch (item) {
+            case 1:
+                tvItemBar1.setEnabled(enable);
+                tvItemText1.setEnabled(enable);
+                btnItemMenu1.setEnabled(enable);
+
+                break;
+            case 2:
+                tvItemBar2.setEnabled(enable);
+                tvItemText2.setEnabled(enable);
+                btnItemMenu2.setEnabled(enable);
+
+                break;
+            case 3:
+                tvItemBar3.setEnabled(enable);
+                tvItemText3.setEnabled(enable);
+                btnItemMenu3.setEnabled(enable);
+
+                break;
+        }
     }
 
     public boolean onPrepareOptionsMenu(Menu menu) {
@@ -87,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    public void setCheckIcon (MenuItem item) {
+    private void setCheckIcon (MenuItem item) {
 
         SpannableStringBuilder builder = new SpannableStringBuilder(getTitleForBuilder(item));
         boolean check = item.isChecked();
@@ -109,4 +152,9 @@ public class MainActivity extends AppCompatActivity {
         else return "* " + title;
     }
 
+    @Override
+    public void onMenuVisibilityChanged(boolean isVisible) {
+        if (isVisible) Toast.makeText(this, "visible", Toast.LENGTH_SHORT).show();
+        else Toast.makeText(this, "unvisible", Toast.LENGTH_SHORT).show();
+    }
 }
