297de59610f96b127ec8c5b8823f76e3429e94e8
Andrew Zayatz <zayatzandrew@mail.ru>
2016-03-05 10:23:47 +0200

    custom actoin bar

---
+++ app/src/main/java/com/zayatz/homework_7/Constant.java
@@ -0,0 +1,8 @@
+package com.zayatz.homework_7;
+
+/**
+ * Created by Zayatz on 05.03.2016.
+ */
+public class Constant {
+    public static final String EXTRA_ITEM_STATE = "item_id";
+}
--- app/src/main/java/com/zayatz/homework_7/MainActivity.java
+++ app/src/main/java/com/zayatz/homework_7/MainActivity.java
@@ -1,13 +1,14 @@
 package com.zayatz.homework_7;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.ImageSpan;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,14 +25,69 @@ public class MainActivity extends AppCompatActivity {
         MenuItem item2 = menu.findItem(R.id.ab_item2);
         MenuItem item3 = menu.findItem(R.id.ab_item3);
 
-        setCheckIcon(item1);
-        setCheckIcon(item2);
-        setCheckIcon(item3);
+        Log.d("TAG", "onCreate");
+        //uncheckOther(item1, item2, item3);
 
         return true;
     }
 
-    private void setCheckIcon (MenuItem item) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.ab_item1:
+                item.setChecked(true);
+                sendStateByIntent(item);
+                return true;
+
+            case R.id.ab_item2:
+                item.setChecked(true);
+                sendStateByIntent(item);
+                return true;
+
+            case R.id.ab_item3:
+                item.setChecked(true);
+                sendStateByIntent(item);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sendStateByIntent (MenuItem item) {
+        Intent intent = new Intent();
+        intent.putExtra(Constant.EXTRA_ITEM_STATE, true);
+        item.setIntent(intent);
+    }
+
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        Log.d("TAG", "onPrepare");
+
+        MenuItem item1 = menu.findItem(R.id.ab_item1);
+        MenuItem item2 = menu.findItem(R.id.ab_item2);
+        MenuItem item3 = menu.findItem(R.id.ab_item3);
+
+        if (item1.getIntent() != null) {
+            item2.setChecked(false);
+            item3.setChecked(false);
+        } else if (item2.getIntent() != null) {
+            item1.setChecked(false);
+            item3.setChecked(false);
+        } else if (item3.getIntent() != null) {
+            item1.setChecked(false);
+            item2.setChecked(false);
+        }
+
+        item1.setIntent(null);
+        item2.setIntent(null);
+        item3.setIntent(null);
+
+        setCheckIcon(item1);
+        setCheckIcon(item2);
+        setCheckIcon(item3);
+        return true;
+    }
+
+    public void setCheckIcon (MenuItem item) {
 
         SpannableStringBuilder builder = new SpannableStringBuilder(getTitleForBuilder(item));
         boolean check = item.isChecked();
@@ -43,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
             builder.setSpan(new ImageSpan(this, R.drawable.ic_unchecked), 0, 1,
                     Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
+
         item.setTitle(builder);
     }
 
@@ -50,30 +107,6 @@ public class MainActivity extends AppCompatActivity {
         String title = String.valueOf(item.getTitle());
         if (title.charAt(0) == '*') return title;
         else return "* " + title;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.ab_item1:
-                if (!item.isChecked()) item.setChecked(true);
-                else item.setChecked(false);
-                setCheckIcon(item);
-                return true;
-
-            case R.id.ab_item2:
-                if (!item.isChecked()) item.setChecked(true);
-                else item.setChecked(false);
-                setCheckIcon(item);
-                return true;
-
-            case R.id.ab_item3:
-                if (!item.isChecked()) item.setChecked(true);
-                else item.setChecked(false);
-                setCheckIcon(item);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
